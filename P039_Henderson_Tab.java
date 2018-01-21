e6b11add865c22019257fbc7a097e3e9feb83707
christopher-henderson <chris@chenderson.org>
2018-01-21 10:42:01 -0700

    fixed class call name

--- P039_Henderson_Tab.java
+++ P039_Henderson_Tab.java
@@ -6,8 +6,7 @@ public class P039_Henderson_Tab extends JPanel {
 	public P039_Henderson_Tab() {
 		this.setLayout(new GridLayout(5, 5));
 		for (int i = 0; i < 25; i ++) {
-			this.add(new P01_Dang_Panel(i));
+			this.add(new P01_CHANGEME_Panel(i));
 		}
-
 	}
 }
